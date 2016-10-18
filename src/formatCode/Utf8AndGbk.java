196e1c88df5081a6d18810f1411353a4e7e81907
caojingkai <971927456@qq.com>
2016-10-18 10:55:59 +0800

    four

--- src/formatCode/Utf8AndGbk.java
+++ src/formatCode/Utf8AndGbk.java
@@ -7,7 +7,7 @@ public class Utf8AndGbk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      try {
-    	 String t = "这是一个字符串aa111123";
+    	 String t = "这是一个字符串aa11121123";
 		String gbk = new String(t.getBytes("UTF-8"),"GBK");
 		System.out.println(gbk);
 		String urf = new String(gbk.getBytes("GBK"),"UTF-8");
